798235b82a19adfd14b2200b8a43d6c0a0b2afc2
Warwick Harris <warwick.harris@yahoo.com.au>
2023-02-14 08:40:44 +1100

    Checking pcsx2 perf

---
+++ images/steam-emu/scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+source /opt/gow/bash-lib/utils.sh
+
+gow_log "Starting Steam with DISPLAY=${DISPLAY}"
+
+# Recursively creating Steam necessary folders (https://github.com/ValveSoftware/steam-for-linux/issues/6492)
+mkdir -p "$HOME/.steam/ubuntu12_32/steam-runtime"
+
+# Start Steam. Use `sudo` to make sure that group membership gets reloaded
+exec /usr/games/steam
+sleep infinity
